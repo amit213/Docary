739b33d3641e3189cf86bdce1ee470beb81671a6
Jef Claes <jef.claes@gmail.com>
2011-10-10 17:55:44 +0200

    Added AddEntryViewModel.cs

---
+++ Docary/Docary.ViewModels/AddEntryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Docary.Models;
+
+namespace Docary.ViewModels
+{
+    public class AddEntryViewModel
+    {
+        public Entry Entry { get; set; }
+    }
+}
