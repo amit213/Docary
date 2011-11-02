00c5c3c8839d800f8ba9a589d79e3fec291f3102
Jef Claes <jef.claes@gmail.com>
2011-11-02 07:43:03 +0100

    Made the stub 'smaller'.

--- Docary/Docary.ViewModelAssemblers.Test/HomeAssemblerTest.cs
+++ Docary/Docary.ViewModelAssemblers.Test/HomeAssemblerTest.cs
@@ -14,7 +14,7 @@ namespace Docary.ViewModelAssemblers.Test
         [TestMethod()]
         public void Test_AssembleHomeIndexViewModel_Initializes_EntryGroups()
         {
-            var target = new HomeAssembler(GetEntryServiceStub());
+            var target = new HomeAssembler(GetEntryServiceStubForTestingEntryGroups());
 
             var actual = target.AssembleHomeIndexViewModel("1");
 
@@ -24,7 +24,7 @@ namespace Docary.ViewModelAssemblers.Test
         [TestMethod()]
         public void Test_AssembleHomeIndexViewModel_Groups_Entries_Correctly()
         {            
-            var target = new HomeAssembler(GetEntryServiceStub());       
+            var target = new HomeAssembler(GetEntryServiceStubForTestingEntryGroups());       
             
             var actual = target.AssembleHomeIndexViewModel("1");
 
@@ -35,51 +35,27 @@ namespace Docary.ViewModelAssemblers.Test
             Assert.AreEqual(secondEntryGroup.Entries.Count(), 1);
         }
 
-        private EntryServiceStub GetEntryServiceStub()
+        private EntryServiceStub GetEntryServiceStubForTestingEntryGroups()
         {
             var entryServiceStub = new EntryServiceStub();                       
             
             var entries = new List<Entry>()
             {
-                new Entry() {
-                    Tag = new EntryTag() {
-                        Id = 1,
-                        Name = "Work"
-                    },
+                new Entry() {                 
                     CreatedOn = DateTime.Now.Date,
-                    Id = 1,
-                    Location = new Location() {
-                        Id = 1,
-                        Name = "Brussel"
-                    },
+                    Id = 1,                    
                     Description = "Bla",
                     UserId = "1"
                 },
-                new Entry() {
-                    Tag = new EntryTag() {
-                        Id = 1,
-                        Name = "Workout"
-                    },
+                new Entry() {                    
                     CreatedOn = DateTime.Now.Date.AddHours(15),
-                    Id = 2,
-                    Location = new Location() {
-                        Id = 2,
-                        Name = "At home"
-                    },
+                    Id = 2,                    
                     Description = "Bla",
                     UserId = "1"
                 },
-                new Entry() {
-                    Tag = new EntryTag() {
-                        Id = 3,
-                        Name = "Blog"
-                    },
+                new Entry() {                    
                     CreatedOn = DateTime.Now.AddDays(1),
-                    Id = 1,
-                    Location = new Location() {
-                        Id = 2,
-                        Name = "At home"
-                    },
+                    Id = 1,                    
                     Description = "Bla",
                     UserId = "1"
                 }
