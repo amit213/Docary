480852ef850c373087b814c80523da09692cad08
Jef Claes <jef.claes@gmail.com>
2011-12-17 22:13:01 +0100

    Added SqlData.sql

---
+++ Docary/Docary.SqlScripts/SqlData.sql
@@ -0,0 +1,34 @@
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[aspnet_SchemaVersions]
+           ([Feature]
+           ,[CompatibleSchemaVersion]
+           ,[IsCurrentVersion])
+     VALUES
+           ('common'
+           ,1
+           ,1)
+GO
+
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[aspnet_SchemaVersions]
+           ([Feature]
+           ,[CompatibleSchemaVersion]
+           ,[IsCurrentVersion])
+     VALUES
+           ('membership'
+           ,1
+           ,1)
+GO
+
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[aspnet_Applications]
+           ([ApplicationName]
+           ,[LoweredApplicationName]
+           ,[ApplicationId]
+           ,[Description])
+     VALUES
+           ('/'
+           ,'/'
+           ,'EE8FE444-412A-4245-AEB8-E9C53A794349'
+           ,NULL)
+GO
+
+
+
