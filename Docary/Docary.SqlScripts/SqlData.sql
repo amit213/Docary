8ed1849963f6794214ff77b9b93b2b79e7c27b26
Jef Claes <jef.claes@gmail.com>
2011-12-18 16:21:26 +0100

    Added Timelinecolors

--- Docary/Docary.SqlScripts/SqlData.sql
+++ Docary/Docary.SqlScripts/SqlData.sql
@@ -30,5 +30,67 @@ INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[aspnet_Applications]
            ,NULL)
 GO
 
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#9CA66C')
+           
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#626E29')
+           
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#DBC742')
+           
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#A3A087')
+           
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#E3E0CA')
+           
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#CAE0E3')
+           
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#52C2D1')
+
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#3A7178')
+
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#C1DE33')
+           
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#4E5E24')
+           
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#68A0B0')
+
+INSERT INTO [db17587aeaa0d44dd3adb59fb9008ad4c5].[dbo].[TimelineColors]
+           ([Value])
+     VALUES
+           ('#FF0000')
+
+GO
+
 
 
