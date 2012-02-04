6bce495ef8f929bdfabc325944660097ebd790fe
Jef Claes <jef.claes@gmail.com>
2012-02-04 15:45:16 +0000

    Added favicon

--- Docary/Docary/Global.asax.cs
+++ Docary/Docary/Global.asax.cs
@@ -19,7 +19,8 @@ namespace Docary
     {        
         public static void RegisterRoutes(RouteCollection routes)
         {            
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");            
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("favicon.ico");
 
             routes.MapRouteLowercase(
                 "Default", // Route name
